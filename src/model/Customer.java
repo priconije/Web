8f7d40f47ed3172cdf4c853364744e812b5c4413
Ognjen <priconije@gmail.com>
2019-06-23 17:24:25 +0200

    fix Customer merge issue

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -3,14 +3,6 @@ package model;
 import enums.UserRole;
 
 public class Customer extends User {
-
-<<<<<<< HEAD
-	public Customer(String username, String password, String firstName, String lastName, UserRole userRole,
-			String phoneNumber, String email, String registrationDate) {
-		super(username, password, firstName, lastName, userRole, phoneNumber, email, registrationDate);
-		// TODO Auto-generated constructor stub
-	}
-=======
 	//list of ordered items(ad)
 	//list of successully delivered items
 	//list of favourite ads
@@ -22,8 +14,5 @@ public class Customer extends User {
 			String phoneNumber, String email, String registrationDate, String city) {
 		super(username, password, firstName, lastName, userRole, phoneNumber, email, registrationDate, city);
 	}
-	
-	
->>>>>>> 981a7bc4a24b58a554788ef0ed4a0613ad2bfc45
 
 }
